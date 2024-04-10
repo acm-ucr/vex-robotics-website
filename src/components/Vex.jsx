3a2bb5fa68551cba9d1d69a133d3add9c021eafe
Marco Navarro <duvalincus@gmail.com>
2024-04-10 14:25:56 -0700

    no bold, margin uses tailwind classes

--- src/components/Vex.jsx
+++ src/components/Vex.jsx
@@ -8,17 +8,15 @@ const Vex = () => {
       <Image
         src={Placeholder}
         alt="Placeholder"
-        className="size-1/2 ml-[10px] float-right"
+        className="size-1/2 ml-5 mt-2 float-right"
       />
-      <p className="first-line:font-bold text-justify">
+      <p className="text-justify">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam.
-      </p>
-      <p>
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        veniam. Duis aute irure dolor in reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+        id est laborum.
       </p>
     </div>
   );
