63a36415a9366f766c75237096d47a2e759e6201
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-04-10 13:23:51 -0700

    Created Title.jsx file
    Ran the formatting software because git didn't like it before. Again let me know what is missing/how to go about it.

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 const titleText = () => {
-    return <title>ABOUT US</title>;
-  };
-  
-  export default titleText;
-  
+  return <title>ABOUT US</title>;
+};
+
+export default titleText;
