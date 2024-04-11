d8d8678f54e3e79a5bd1994ea489ec55146f27b2
sunbrandon <sunabrandon@gmail.com>
2024-04-11 14:33:15 -0700

    Fixed formatting

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-const Button = (link, text, color) => {
+const Button = ({ link, text, color }) => {
   return (
     <Link
       href={link}
