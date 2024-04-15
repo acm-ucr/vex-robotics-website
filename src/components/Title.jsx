760ccfdf1a56acf920001e1b9899891c4cd71bd0
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-04-15 01:58:32 -0700

    ran the formatter after switching css to tailwind

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Title = () => {
-  return (
-    <div class="font-russo text-white text-[110px]">
-      ABOUT US
-    </div>
-  );
+  return <div class="font-russo text-white text-[110px]">ABOUT US</div>;
 };
 
 export default Title;
