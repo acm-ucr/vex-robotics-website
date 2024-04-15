251c93d0c0a9740df9a14caa0ef80a76e0d3c151
sunbrandon <sunabrandon@gmail.com>
2024-04-14 18:01:30 -0700

    Created subteam component

---
+++ src/components/about/Subteam.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Image from "next/image";
+import Placeholder from "@/public/images/placeholder.webp";
+import SubteamTag from "../SubteamTag";
+
+const Subteam = ({ text }) => {
+  return (
+    <div>
+      <div className="pb-3 max-w-fit">
+        <>
+          <SubteamTag text={"SUBTEAM"} />
+        </>
+      </div>
+      <div className="flex items-center">
+        <Image
+          src={Placeholder}
+          alt="Placeholder"
+          className="size-44 rounded-full float-left"
+        />
+        <p className="pl-4 max-w-xs text-center font-saira">{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Subteam;
