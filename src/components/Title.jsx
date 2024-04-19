471237abe9ed4fdecf493a4259e58922d8844d16
Isaac Castro <74474132+thegta5p@users.noreply.github.com>
2024-04-18 20:54:10 -0700

    run prettier

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "../../public/assets/VEXROBOTICS.svg";
+import Location from "../../public/assets/UCR.svg";
 
 const Title = () => {
   return (
     <div className="">
       <Image src={Logo} alt="VEX Robotics Logo" width={964} height={66} />
       <div className="flex justify-center">
-        <div class="font-russo text-white text-8xl">ABOUT US</div>
+        <Image src={Location} alt="UCR Logo" width={444} height={66} />
       </div>
     </div>
   );
