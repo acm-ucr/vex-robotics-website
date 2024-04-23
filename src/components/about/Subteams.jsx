e0117b2699cb0ea6072f2d7b82edd350c60e1e29
sunbrandon <sunabrandon@gmail.com>
2024-04-23 13:29:31 -0700

    Created Subteams component, added subteams items, added formatting feature to Subteam component

---
+++ src/components/about/Subteams.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Subteam from "./Subteam";
+import { items } from "@/data/subteams";
+
+const Subteams = () => {
+  return (
+    <div>
+      {items.map((item, index) => (
+        <Subteam
+          key={index}
+          text={item.text}
+          img={item.img}
+          float={item.float}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Subteams;
