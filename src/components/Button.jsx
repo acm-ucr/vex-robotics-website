c59fe2c9cb1ec7fd8bd53b5279f9eee2c5953e2d
Marco Navarro <duvalincus@gmail.com>
2024-05-01 11:48:37 -0700

    new default button and README update

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
 
-const Button = ({ link, text, color }) => {
+const Button = ({ link, text, color = "bg-vex-black" }) => {
   return (
     <Link
       href={link}
-      className={`${color} text-white font-righteous py-2 px-4 rounded`}
+      className={`${color} text-white font-righteous py-2 px-4 rounded 
+      border-2 border-vex-purple-300 hover:bg-vex-purple-300`}
     >
       {text}
     </Link>
