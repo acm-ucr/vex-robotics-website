72a90881854b70948c560e59650f37cc8363bbb6
dylanwong6605 <dylanwong6605@gmail.com>
2024-05-03 12:09:01 -0700

    built event page

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -1,7 +1,11 @@
 import Events from "@/components/calendar/Events";
-
+import Calendar from "@/components/calendar/Calendar";
 const EventsPage = () => {
-  return <Events />;
+  return <div className="flex flex-col w-full justify-center items-center py-20">
+    <Calendar/>
+    <Events />
+
+  </div>;
 };
 
 export default EventsPage;
--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -63,7 +63,7 @@ const CalendarEvent = () => {
   }, []);
 
   return (
-    <section className="w-full flex justify-center items-center flex-col mt-[2vh] -mb-28 font-lora">
+    <section className="w-2/3 flex justify-center items-center flex-col mt-[2vh] -mb-28 font-lora bg-vex-white">
       <motion.div
         className="w-10/12 flex justify-center items-center"
         variants={animation}
