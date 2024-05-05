aa86f8af1d3539d1d12a55c563a2436f1cd940f7
SafeDuck <schopra579@gmail.com>
2024-05-04 17:53:13 -0700

    About Page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,12 @@
 import AboutUs from "@/components/about/AboutUs";
-
+import Subteams from "@/components/about/Subteams";
 const AboutPage = () => {
-  return <AboutUs />;
+  return (
+    <div className="flex flex-col w-full justify-center items-center gap-10">
+      <AboutUs />
+      <Subteams />
+    </div>
+  );
 };
 
 export default AboutPage;
--- src/components/about/AboutUs.jsx
+++ src/components/about/AboutUs.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Image from "next/image";
 import Placeholder from "@/public/images/placeholder.webp";
-import Subteam from "./Subteam";
-import Button from "../Button";
 
 const AboutUs = () => {
   return (
     <div className="flex flex-col justify-center items-center text-vex-white">
-      <p className="text-center font-russo text-4xl mb-9">ABOUT US</p>
       <div className="w-2/5 flex flex-col justify-center items-center text-vex-white">
         <Image src={Placeholder} alt="Placeholder" className="mb-11 w-4/5" />
         <p className="text-center font-saira text-xl">
@@ -18,29 +15,6 @@ const AboutUs = () => {
           reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur.
         </p>
-      </div>
-
-      <div className="mr-72 my-12">
-        <Subteam
-          text="Duis aute irure dolor in -reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-        nulla pariatur. Excepteur sint occaecat cupidatat non proident."
-        ></Subteam>
-      </div>
-      <div className="mb-12 ml-[550px]">
-        <Subteam
-          text="Duis aute irure dolor in -reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-        pariatur. Excepteur sint occaecat cupidatat non proident."
-        ></Subteam>
-      </div>
-      <div className="mr-[300px]">
-        <Subteam
-          text="Duis aute irure dolor in -reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-        pariatur. Excepteur sint occaecat cupidatat non proident."
-        ></Subteam>
-      </div>
-
-      <div className="my-12 flex justify-center border-indigo-500 border-2 rounded-lg text-xl">
-        <Button link="/" text="Contact Us" color="bg-vex-black" />
       </div>
     </div>
   );
