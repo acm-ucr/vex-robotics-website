0b7dfc0c8f500ed433d7898b2a32b7e54bd34ecb
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-05-05 14:30:41 -0700

    Fixed the .svg import file
    Got the svg file loading properly and now the component is ready for use

--- src/components/about/AboutHeader.jsx
+++ src/components/about/AboutHeader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import PurpleBar from "@/public/assetsPurpleBar.svg";
+import PurpleBar from "../../../public/assets/PurpleBar.svg";
 import Image from "next/image";
 
 const AboutHeader = () => {
@@ -7,7 +7,7 @@ const AboutHeader = () => {
     <div className="flex flex-col justify-center items-center">
       <div className=" text-vex-white font-russo text-8xl">ABOUT US</div>
       <div>
-        <Image src={PurpleBar} alt="email-logo" />
+        <Image src={PurpleBar} alt="purple-bar" />
       </div>
     </div>
   );
