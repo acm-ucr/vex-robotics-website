5476329ddd3e6c7e33d7f7c8aa8b11ada38bc871
Marco Navarro <duvalincus@gmail.com>
2024-05-08 19:05:24 -0700

    Revert "changed footer icons to map through footer.js, attempted responsiveness"
    This reverts commit fc149ccb2770e3f88d48426e09635ad16178d912.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,37 +1,30 @@
 import React from "react";
 import RightPattern from "@/public/assets/RightPattern.svg";
 import Logo from "../../public/assets/logo.webp";
+import InstagramLogo from "../../public/assets/InstagramLogo.svg";
+import DiscordLogo from "../../public/assets/DiscordLogo.svg";
+import EmailLogo from "../../public/assets/EmailLogo.svg";
+import LinkedInLogo from "../../public/assets/LinkedInLogo.svg";
 import BottomPattern from "../../public/assets/BottomPattern.svg";
 import LeftPattern from "../../public/assets/LeftPattern.svg";
 import Image from "next/image";
-import { ICONS } from "../data/footer";
-import Link from "next/link";
-
 const Footer = () => {
   return (
     <div className="bg-vex-black w-full">
-      <div className="flex justify-between flex-row items-center">
-        <div className="md:mr-10 md:w-1/3 w-1/4">
+      <div className="flex justify-between flex-row">
+        <div className="ml-10 w-1/4">
           <Image src={LeftPattern} alt="left-pattern" />
         </div>
-        <div className="w-1/2 md:1/3">
-          <div className="bg-vex-purple-400 flex flex-col items-center justify-center rounded-lg">
-            <div className="w-7/12 md:w-2/5 ">
-              <Image src={Logo} alt="logo" className="p-2" />
+        <div className="w-1/4">
+          <div className="bg-vex-purple-400 grid justify-items-center grid-rows-2 justify-center pt-10 rounded-lg">
+            <div className="w-11 md:w-24">
+              <Image src={Logo} alt="logo" />
             </div>
-            <div className="flex flex-row justify-around items-center w-4/5 pb-7 h-2/5">
-              {ICONS.map((icon, index) => {
-                return (
-                  <Link
-                    key={index}
-                    href={icon.link}
-                    target="_blank"
-                    className="hover:scale-110 duration-300 md:text-6xl sm:text-3xl text-xl text-white"
-                  >
-                    {icon.logo}
-                  </Link>
-                );
-              })}
+            <div className="flex flex-row justify-center w-3 md:w-9 gap-x-2 md:gap-x-8">
+              <Image src={InstagramLogo} alt="instagram-logo" />
+              <Image src={DiscordLogo} alt="discord-logo" />
+              <Image src={EmailLogo} alt="email-logo" />
+              <Image src={LinkedInLogo} alt="linkedin-logo" />
             </div>
           </div>
           <div className="flex flex-row justify-between mx-10">
@@ -43,7 +36,7 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="md:ml-10 w-1/4 md:w-1/3">
+        <div className="mr-10 w-1/4">
           <Image src={RightPattern} alt="right-pattern" />
         </div>
       </div>
