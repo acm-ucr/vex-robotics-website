db113c84df4d752a04f11d4c29a184cf1e275161
Marco Navarro <duvalincus@gmail.com>
2024-05-08 19:11:03 -0700

    no responsiveness, only icons and data

---
+++ src/data/footer.js
@@ -0,0 +1,22 @@
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaDiscord, FaInstagram } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
+
+export const ICONS = [
+  {
+    link: "https://www.instagram.com",
+    logo: <FaInstagram />,
+  },
+  {
+    link: "https://www.discord.com",
+    logo: <FaDiscord />,
+  },
+  {
+    link: "mailto::dummy@gmail.com",
+    logo: <IoMail />,
+  },
+  {
+    link: "https://www.linkedin.com",
+    logo: <AiFillLinkedin />,
+  },
+];
