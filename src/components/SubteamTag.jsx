61e8d426a075cf8b44906670bc33beb234335455
Nimrah Saleem <nimsaleem44@gmail.com>
2024-05-08 19:09:40 -0700

    home

--- src/components/SubteamTag.jsx
+++ src/components/SubteamTag.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-//  how to see this on the website?
-
 const SubteamTag = ({ text }) => {
   return (
     <div className="rounded-full bg-purple-500 px-4 py-3 text-3xl font-russo text-vex-white">
--- src/data/navigation.js
+++ src/data/navigation.js
@@ -1,7 +1,7 @@
 export const items = [
   {
     name: "HOME",
-    link: "/home",
+    link: "/",
   },
   {
     name: "ABOUT US",
