615244028772f0a30cf670eba1afef711bb228ab
SafeDuck <schopra579@gmail.com>
2024-05-09 19:38:45 -0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import PurpleBar from "../../../public/assets/PurpleBar.svg";
+import Image from "next/image";
 
-const Header = () => {
-  return <div>Header</div>;
+const Header = ({ text }) => {
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className=" text-vex-white font-russo text-8xl">{text}</div>
+      <div>
+        <Image src={PurpleBar} alt="purple-bar" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
