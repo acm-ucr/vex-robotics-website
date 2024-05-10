c974e7b3f6d71f2c5f5a23d5cf46d05746f722e9
Sweden Agunenye <swedenagu@ucr-secure-39-10-13-207-15.wnet.ucr.edu>
2024-05-10 14:13:41 -0700

    built media page

--- src/components/media/Medias.jsx
+++ src/components/media/Medias.jsx
@@ -4,7 +4,7 @@ import { medias } from "@/data/medias";
 
 const Medias = () => {
   return (
-    <div className="grid grid-cols-2 justify-items-center items-center w-3/4 gap-14">
+    <div className="grid grid-cols-2 justify-items-center items-center w-1/2 gap-14">
       {medias.map((item, index) => (
         <Media
           key={index}
