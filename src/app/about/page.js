aac77c5aace5494b2baa26eeabf924fcbba27bf7
Marco Navarro <duvalincus@gmail.com>
2024-05-10 11:55:32 -0700

    Added purple bar to bottom of each page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,10 +1,13 @@
 import AboutUs from "@/components/about/AboutUs";
 import Subteams from "@/components/about/Subteams";
+import Image from "next/image";
+import purpleBar from "@/public/assets/PurpleBar.svg";
 const AboutPage = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-10">
       <AboutUs />
       <Subteams />
+      <Image src={purpleBar} alt="purple bar" className="w-3/4" />
     </div>
   );
 };
