1bf2546e48648ae68e353fc76b1fa2bbb3ff785f
sunbrandon <sunabrandon@gmail.com>
2024-05-10 17:19:52 -0700

    Fix merge conflict

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,11 @@ import Landing from "@/components/landing/Landing";
 import Vex from "@/components/Vex";
 import Sponsors from "@/components/home/Sponsors";
 import Companies from "@/components/Companies";
-import Subteams from "@/components/about/Subteams";
 const Home = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-10">
       <Landing />
       <Vex />
-      <Subteams />
       <Sponsors />
       <Companies />
     </div>
