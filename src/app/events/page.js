a672f727179bc0938ef3a94289219e7ce0de2c1f
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-05-17 11:36:31 -0700

    Media Header

--- src/app/events/page.js
+++ src/app/events/page.js
@@ -2,10 +2,12 @@ import Events from "@/components/calendar/Events";
 import Calendar from "@/components/calendar/Calendar";
 import Image from "next/image";
 import purpleBar from "@/public/assets/PurpleBar.svg";
+import Header from "@/components/Header";
 
 const EventsPage = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center py-20 gap-10">
+      <Header text="EVENTS" />
       <Calendar />
       <Events />
       <Image alt="bottom pattern" src={purpleBar} className="w-3/4" />
--- src/app/media/page.js
+++ src/app/media/page.js
@@ -1,9 +1,11 @@
+import Header from "@/components/Header";
 import Media from "@/components/media/Media";
 import Medias from "@/components/media/Medias";
 
 const MediaPage = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-y-5">
+      <Header text="MEDIA" />
       <Media />
       <Medias />
     </div>
