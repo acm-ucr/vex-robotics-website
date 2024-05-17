bf7c3c2fd0516f29d7bf1e54a23012a01a39e772
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-05-17 12:11:17 -0700

    animated and fixed button

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -1,3 +1,6 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
 import Medias from "@/components/media/Medias";
 import Button from "@/components/Button";
 
@@ -5,7 +8,13 @@ const MediaPage = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-y-5">
       <Medias />
-      <Button link="/contact" text="Contact Us" color="" />
+      <motion.div
+        className="pb-2"
+        whileHover={{ scale: 1.1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 30 }}
+      >
+        <Button link="/contact" text="Contact Us" />
+      </motion.div>
     </div>
   );
 };
