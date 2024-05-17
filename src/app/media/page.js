b4f40504bc2842e0e17b570b36d6744e05763883
Gabe H <162786819+GitHaresco@users.noreply.github.com>
2024-05-17 10:16:24 -0700

    added button to media

--- src/app/media/page.js
+++ src/app/media/page.js
@@ -1,11 +1,13 @@
 import Media from "@/components/media/Media";
 import Medias from "@/components/media/Medias";
+import Button from "@/components/Button";
 
 const MediaPage = () => {
   return (
     <div className="flex flex-col w-full justify-center items-center gap-y-5">
       <Media />
       <Medias />
+      <Button text="Contact Us" href="/" />
     </div>
   );
 };
