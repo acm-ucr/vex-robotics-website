23852387032f776fe80b6607e053b3ac690a3094
Nimrah Saleem <nimsaleem44@gmail.com>
2024-05-17 11:45:12 -0700

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,9 @@ import Image from "next/image";
 const Header = ({ text }) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <div className=" text-vex-white font-russo text-8xl">{text}</div>
+      <div className=" text-vex-white font-russo text-4xl p-2 md:text-8xl md:p-8">
+        {text}
+      </div>
       <div>
         <Image src={PurpleBar} alt="purple-bar" />
       </div>
