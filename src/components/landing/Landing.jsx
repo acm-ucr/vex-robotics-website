ac7f688e76c2c8203e56b245e1a7433cedb05f09
dylanwong6605 <dylanwong6605@gmail.com>
2024-05-24 02:09:30 -0700

    Animated Landing

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,7 +1,9 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Title from "@/components/Title";
 import Button from "@/components/Button";
+import { motion } from "framer-motion";
 const Landing = () => {
   return (
     <div className="flex flex-col justify-center items-center">
@@ -15,11 +17,22 @@ const Landing = () => {
         />
       </div>
       <div className="flex flex-col justify-center items-center h-screen md:w-full w-[70%] ">
-        <Title />
-
-        <div className="flex justify-center mt-10 hover:-translate-y-1 duration-300">
-          <Button link={"/"} text={"Join Us"} color={"bg-vex-purple-300"} />
-        </div>
+        <motion.div
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeIn", delay: 0.2 }}
+        >
+          <Title />
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.7, ease: "easeIn", delay: 0.2 }}
+        >
+          <div className="flex justify-center mt-10 hover:-translate-y-1 duration-300">
+            <Button link={"/"} text={"Join Us"} color={"bg-vex-purple-300"} />
+          </div>
+        </motion.div>
       </div>
     </div>
   );
